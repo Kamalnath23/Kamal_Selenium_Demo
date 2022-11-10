e95e4c2748dab4417c83ffc89cdd81d168256b5b
Kamalnath <kamalnath121998@gmail.vom>
2022-11-10 21:29:04 +0530

    American ARCH Commit

--- SeleniumFrameworkProject7/src/test/java/KamalIndustries/Tests/GIT_DemoTest.java
+++ SeleniumFrameworkProject7/src/test/java/KamalIndustries/Tests/GIT_DemoTest.java
@@ -7,4 +7,10 @@ public class GIT_DemoTest {
 	public void demo() {
 		System.out.println("REMOTE ARCHITECT GIT DEMO BY INDIAN TIME ZONE Guy");
 	}
+	
+	//american test
+	public void ironMan() {
+		System.out.println("IRONMAN ARMOURED ADVENTURES");
+		System.out.println("\n This was developed by US TIME ZONE ARCH");
+	}
 }
